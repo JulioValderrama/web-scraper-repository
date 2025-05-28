a66cd49e1d25a1c82766bf804f72d817eef86582
JulioValdem <juliovaldem@gmail.com>
2025-05-28 10:18:56 +0000

    Backup workflows 20250528_101848

--- scripts/export_workflows.sh
+++ scripts/export_workflows.sh
@@ -12,11 +12,7 @@ ls -tp $WORKFLOW_DIR/export_*.json | grep -v '/$' | tail -n +11 | xargs -I {} rm
 # Exporta todos los workflows en un único archivo
 docker exec n8n-main n8n export:workflow --all --output="/home/node/exported_workflows/export_${TIMESTAMP}.json"
 
-# Copia el backup desde el contenedor al host
-docker cp n8n-main:/home/node/exported_workflows/. $WORKFLOW_DIR/
-
 # Commit local a git
 cd /opt/n8n
 git add workflows
 git commit -m "Backup workflows $TIMESTAMP"
-
